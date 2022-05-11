08e7b6865af60ee6e639e557157f122fd1c93a1a
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 08:56:03 -0600

    update

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
   <head>
     <title>first Coding steps assignment: Hello world</title>
   </head>
-  <body><?php echo "Hello world!";?></body>
+  <body><?php echo "Hello world!";
+  //contains hello world! being echoed by PHP to the browser ?></body>
 </html>
